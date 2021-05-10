5acef56bbe801047c3f424f75fb43da9bf4526ad
unknown <2462177270@qq.com>
2021-05-10 22:06:36 +0800

    v0.4

--- basic-server/realize-service/rserver.go
+++ basic-server/realize-service/rserver.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"simple-farme/basic-server/abstract-interface"
+	"simple-farme/basic-server/utils"
 )
 
 //the realize of server interface,define a server struct
@@ -23,11 +24,14 @@ type Server struct {
 
 //start server
 func (s *Server) Start()  {
-	fmt.Printf("Server Listenner at IP:%s,Port%d,is starting\n",s.IP,s.Port)
+	fmt.Printf("Server Name: %s, listenner at IP: %s,Port:%d is starting",
+		utils.GlobalObject.Name,utils.GlobalObject.Host,utils.GlobalObject.TcpPort)
+	fmt.Sprintf("framework version :%s,Maxconn:%d,MaxPackageSize:%d\n",
+		utils.GlobalObject.Version,utils.GlobalObject.MaxConn,utils.GlobalObject.MaxPackageSize)
 	//if it's not use go func,then start() will be always block
 	go func() {
 		//get a TCP addr
-		addr,err:=net.ResolveTCPAddr(s.IPVersion,fmt.Sprint("%s:%d",s.IP,s.Port))
+		addr,err:=net.ResolveTCPAddr(s.IPVersion,fmt.Sprint("%s,%d",s.IP,s.Port))
 		if err !=nil{
 			fmt.Println("addr error",err)
 			return
@@ -73,10 +77,10 @@ func (s *Server) AddRouter(router abstract_interface.ARouter){
 //initialize server
 func NewServer(name string) abstract_interface.AServer{
 	s:=&Server{
-		Name: name,
+		Name: utils.GlobalObject.Name,
 		IPVersion: "tcp4",
-		IP: "0.0.0.0",
-		Port: 8554,
+		IP: utils.GlobalObject.Host,
+		Port: utils.GlobalObject.TcpPort,
 		Router:nil,
 	}
 	return s
---
+++ basic-server/utils/globalobj.go
@@ -0,0 +1,53 @@
+package utils
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"simple-farme/basic-server/abstract-interface"
+)
+
+//stores global arguments
+//configured by the user with JSON
+type GlobalObj struct {
+	//global obj
+	TcpServer abstract_interface.AServer
+	//IP
+	Host string
+	//port
+	TcpPort int
+	//server name
+	Name string
+	//framework version
+	Version string
+	//Max connection
+	MaxConn int
+	//MAX data package
+	MaxPackageSize uint32
+}
+//define global obj
+var GlobalObject *GlobalObj
+//reload from json
+func (g *GlobalObj) Reload(){
+	data,err :=ioutil.ReadFile("")
+	if err !=nil{
+		panic(err)
+	}
+	//resolve to struct
+	err=json.Unmarshal(data,&GlobalObject)
+	if err !=nil{
+		panic(err)
+	}
+}
+func init(){
+	//default value
+	GlobalObject=&GlobalObj{
+		Name:"xxx",
+		Version: "v0.4",
+		TcpPort: 8999,
+		Host: "0.0.0.0",
+		MaxConn: 1000,
+		MaxPackageSize:4096,
+	}
+	//load from json
+	GlobalObject.Reload()
+}
